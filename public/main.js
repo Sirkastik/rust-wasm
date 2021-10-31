61598aeb98f3321a44b70a0a11ad7aa5dbb56f2f
Sarkastik <riobavictor2019@gmail.com>
2021-10-31 23:33:27 +0300

    build finished **

--- public/main.js
+++ public/main.js
@@ -11,32 +11,19 @@ const init = async () => {
     console.log(rustApp);
 
     const input = document.getElementById('upload')
-    let effect = document.getElementById('effect')
     const fileReader = new FileReader()
-    let operation;
 
     fileReader.onloadend = () => {
         let base64 = fileReader.result.replace(
             /^data:image\/(png|jpeg|jpg);base64,/, ''
         )
-        let img_data_url;
-
-        if (operation == 'grayscale') {
-            img_data_url = rustApp.grayscale(base64);
-        } else if (operation == 'brighten') {
-            img_data_url = rustApp.brighten(base64, 10);
-        } else {
-            console.log(operation)
-        }
+        let img_data_url = rustApp.grayscale(base64);
         document.getElementById('new-img')
             .setAttribute('src', img_data_url)
     }
 
     input.addEventListener('change', () => {
         fileReader.readAsDataURL(input.files[0])
-    })
-    effect.addEventListener('change', () => {
-        operation = effect.value;
     })
 }
 
