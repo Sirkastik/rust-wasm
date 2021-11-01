e6420c42c982159b5dcca6a8a460db274ef0f258
Sarkastik <riobavictor2019@gmail.com>
2021-11-01 14:10:52 +0300

    added effects

--- public/main.js
+++ public/main.js
@@ -1,6 +1,5 @@
 const init = async () => {
     let rustApp = null
-
     try {
         rustApp = await import('../pkg')
     } catch (e) {
@@ -8,23 +7,105 @@ const init = async () => {
         return;
     }
 
-    console.log(rustApp);
-
-    const input = document.getElementById('upload')
+    const file_input = document.getElementById('upload')
+    const effect = document.getElementById('effect')
+    const submit_btn = document.getElementById('submit')
+    const slider = document.getElementById("myRange");
+    const sliderbox = document.getElementById("slider");
     const fileReader = new FileReader()
+    const image = document.getElementById('new-img')
 
-    fileReader.onloadend = () => {
-        let base64 = fileReader.result.replace(
-            /^data:image\/(png|jpeg|jpg);base64,/, ''
-        )
-        let img_data_url = rustApp.grayscale(base64);
+    let effect_value = document.getElementById("value");
+    effect_value.innerHTML = slider.value;
+
+
+    const down_btn = document.getElementById('download')
+    const link = document.getElementById('link')
+
+    let base64;
+
+    file_input.onchange = () => {
+        fileReader.readAsDataURL(file_input.files[0])
         document.getElementById('new-img')
-            .setAttribute('src', img_data_url)
+            .setAttribute('src', window.URL.createObjectURL(file_input.files[0]))
     }
 
-    input.addEventListener('change', () => {
-        fileReader.readAsDataURL(input.files[0])
-    })
+    fileReader.onloadend = () => {
+        base64 = fileReader.result.replace(
+            /^data:image\/(png|jpeg|jpg);base64,/, ''
+        )
+    }
+
+    effect.onchange = () => {
+        if (effect.value == 'blur') {
+            sliderbox.classList.remove('hidden')
+            slider.max = 3
+            slider.step = .1
+            slider.value = 1.5
+            effect_value.innerHTML = slider.value;
+        } else if (effect.value == 'brighten') {
+            sliderbox.classList.remove('hidden')
+            slider.max = 50
+            slider.step = 1
+            slider.value = 25
+            effect_value.innerHTML = slider.value;
+        } else {
+            sliderbox.classList.add('hidden')
+        }
+    }
+
+    slider.oninput = () => {
+        effect_value.innerHTML = slider.value;
+    }
+
+    link.onclick = () => {
+        link.download = `img-${effect.value}.png`;
+        down_btn.removeChild(link);
+        down_btn.classList.add("hidden")
+        image.src = ''
+        delete link;
+    }
+
+    submit_btn.onclick = () => {
+        if (base64) {
+            let img_data_url = process_img(rustApp, base64, effect.value, slider.value);
+            image.src = img_data_url
+            down_btn.classList.remove("hidden")
+            link.href = img_data_url;
+        } else {
+            alert('Please upload image')
+        }
+    }
+}
+
+const process_img = (rustApp, base64, effect, value) => {
+    let img_data_url;
+    switch (effect) {
+        case 'grayscale':
+            img_data_url = rustApp.grayscale(base64);
+            break;
+        case 'rotate_90':
+            img_data_url = rustApp.rotate_90(base64);
+            break;
+        case 'rotate_180':
+            img_data_url = rustApp.rotate_180(base64);
+            break;
+        case 'rotate_270':
+            img_data_url = rustApp.rotate_270(base64);
+            break;
+        case 'blur':
+            img_data_url = rustApp.blur(base64, parseFloat(value));
+            break;
+        case 'brighten':
+            img_data_url = rustApp.brighten(base64, parseInt(value));
+            break;
+        case 'crop':
+            img_data_url = rustApp.crop(base64, 0, 0, 320, 180);
+            break;
+        default:
+            img_data_url = rustApp.grayscale(base64);
+    }
+    return img_data_url
 }
 
 init();
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-use wasm_bindgen::prelude::*;
-use web_sys::console::log_1 as log;
-use base64::{ encode, decode };
+use base64::{decode, encode};
 use image::load_from_memory;
 use image::ImageOutputFormat::Png;
-
+use wasm_bindgen::prelude::*;
+use web_sys::console::log_1 as log;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) -> String {
@@ -19,14 +18,149 @@ pub fn grayscale(encoded_file: &str) -> String {
     log(&"...Grayscale effect applied".into());
 
     let mut buffer = vec![];
-    img.write_to(& mut buffer, Png).unwrap();
+    img.write_to(&mut buffer, Png).unwrap();
     log(&"...New image written".into());
 
     let encoded_img = encode(&buffer);
-    let data_url = format!(
-        "data:image/png;base64,{}",
-        encoded_img
-    );
+    let data_url = format!("data:image/png;base64,{}", encoded_img);
+
+    data_url
+}
+
+#[wasm_bindgen]
+pub fn brighten(encoded_file: &str, brighten_value: i32) -> String {
+    log(&"Brighten called...".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"...Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"...Image loaded".into());
+
+    img = img.brighten(brighten_value);
+    log(&"...Brighten effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"...New image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}", encoded_img);
+
+    data_url
+}
+
+#[wasm_bindgen]
+pub fn blur(encoded_file: &str, blur_sigma: f32) -> String {
+    log(&"Blur called...".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"...Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"...Image loaded".into());
+
+    img = img.blur(blur_sigma);
+    log(&"...Blur effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"...New image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}", encoded_img);
+
+    data_url
+}
+
+#[wasm_bindgen]
+pub fn rotate_90(encoded_file: &str) -> String {
+    log(&"Rotate90 called...".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"...Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"...Image loaded".into());
+
+    img = img.rotate90();
+    log(&"...Rotate90 effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"...New image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}", encoded_img);
+
+    data_url
+}
+
+#[wasm_bindgen]
+pub fn rotate_180(encoded_file: &str) -> String {
+    log(&"Rotate180 called...".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"...Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"...Image loaded".into());
+
+    img = img.rotate180();
+    log(&"...Rotate180 effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"...New image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}", encoded_img);
+
+    data_url
+}
+
+#[wasm_bindgen]
+pub fn rotate_270(encoded_file: &str) -> String {
+    log(&"Rotate270 called...".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"...Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"...Image loaded".into());
+
+    img = img.rotate270();
+    log(&"...Rotate270 effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"...New image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}", encoded_img);
+
+    data_url
+}
+
+#[wasm_bindgen]
+pub fn crop(encoded_file: &str, x: u32, y: u32, width: u32, height: u32) -> String {
+    log(&"Crop called...".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"...Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"...Image loaded".into());
+
+    img = img.crop(x, y, width, height);
+    log(&"...Crop effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"...New image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}", encoded_img);
 
     data_url
 }
